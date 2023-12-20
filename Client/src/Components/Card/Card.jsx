95c19ec6cd33758bb2eee233dc7173223f593df9
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2023-12-20 19:42:28 +0100

    Colores para Species y Status

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -12,12 +12,53 @@ function Card(props) {
   const [isFav, setIsFav] = useState(false);
   const [closeBtn, setCloseBtn] = useState(true);
 
+  // Variable para el color de la especie.
+  let speciesClass;
+
+  switch (character.species) {
+    case "Human":
+      speciesClass = style.human;
+      break;
+    case "Animal":
+      speciesClass = style.animal;
+      break;
+    case "Alien":
+      speciesClass = style.alien;
+      break;
+    case "Robot":
+      speciesClass = style.robot;
+      break;
+    case "Humanoid":
+      speciesClass = style.humanoid;
+      break;
+    case "Mythological Creature":
+      speciesClass = style.mythological;
+      break;
+    case "Poopybutthole":
+      speciesClass = style.poopy;
+      break;
+    default:
+      speciesClass = style.unknown
+      break;
+  }
+
+  // Variable para el color del status.
+  let colorClass;
+
+  if (character.status === "Alive") {
+    colorClass = style.green;
+  } else if (character.status === "Dead") {
+    colorClass = style.red;
+  } else if (character.status === "unknown") {
+    colorClass = style.yellow;
+  }
+
   useEffect(() => {
-   if (!onClose) {
-     setCloseBtn(false);
-   }
- }, []);
-  
+    if (!onClose) {
+      setCloseBtn(false);
+    }
+  }, []);
+
   const handleFavorite = (character) => {
     if (!isFav) {
       addFav(character);
@@ -40,13 +81,14 @@ function Card(props) {
     <div className={style.cardContainer}>
       <span className={style.characterName}>{character.name}</span>
       <div className={style.imageContainer}>
-        <img src={character.image} alt="" />
+        <img src={character.image} alt='' />
       </div>
+      <span className={style.charID}>#{character.id}</span>
       {closeBtn && (
         <button
-        onClick={() => {
-          onClose(character.id);
-        }}
+          onClick={() => {
+            onClose(character.id);
+          }}
         >
           X
         </button>
@@ -71,8 +113,12 @@ function Card(props) {
         </button>
       )}
       <div className={style.contentContainer}>
-        <p>Species: <span>{character.species}</span></p>
-        <p>Status: <span>{character.status}</span></p>
+        <p>
+          Species: <span className={speciesClass}>{character.species}</span>
+        </p>
+        <p>
+          Status: <span className={colorClass}>{character.status}</span>
+        </p>
       </div>
     </div>
   );
