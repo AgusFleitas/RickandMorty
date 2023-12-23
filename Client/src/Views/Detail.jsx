9f8036bd3d2ed7dec4bc5d5967c6a8c4d2293925
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2023-12-23 03:23:04 +0100

    Detail Finalizado

--- Client/src/Views/Detail.jsx
+++ Client/src/Views/Detail.jsx
@@ -1,29 +1,75 @@
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+import homeVideo from "../video/home.mp4";
+
+import style from "./Detail.module.css";
 
 const Detail = () => {
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
 
-    const {id} = useParams()
-    const [character, setCharacter] = useState({})
+  useEffect(() => {
+    axios(`http://localhost:3001/character/${id}`).then(({ data }) => {
+      if (data.name) {
+        setCharacter(data);
+      } else {
+        window.alert("Hubo un error al cargar el Detail.");
+      }
+    });
+    return setCharacter({});
+  }, [id]);
 
-    useEffect(() => {
-        axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
-           if (data.name) {
-              setCharacter(data);
-           } else {
-              window.alert('No hay personajes con ese ID');
-           }
-        });
-        return setCharacter({});
-     }, [id]);
-
-    return (
-        <div>
-         <h1>Esto es el Detail.</h1>
+  return (
+    <div className={style.detail}>
+      <h1 className={style.title}>DETAILS ABOUT THE CHARACTER</h1>
+      <div class={style.wrapper}>
+        <h2>{character.name}</h2>
+        <img src={character.image} alt='' />
+        <div class={style.otherdata}>
+          <span>ID: </span>
+          <div class={style.valuebox}>
+            <span>#{character.id}</span>
+          </div>
         </div>
-    )
-}
+        <div class={style.otherdata}>
+          <span>STATUS: </span>
+          <div class={style.valuebox}>
+            <span>{character.status}</span>
+          </div>
+        </div>
+        <div class={style.otherdata}>
+          <span>SPECIES: </span>
+          <div class={style.valuebox}>
+            <span>{character.species}</span>
+          </div>
+        </div>
+        <div class={style.otherdata}>
+          <span>GENDER: </span>
+          <div class={style.valuebox}>
+            <span>{character.gender}</span>
+          </div>
+        </div>
+        <div class={style.otherdata}>
+          <span>ORIGIN: </span>
+          <div class={style.valuebox}>
+            <span>{character.origin}</span>
+          </div>
+        </div>
+        <div class={style.otherdata}>
+          <span>LOCATION: </span>
+          <div class={style.valuebox}>
+            <span>{character.location}</span>
+          </div>
+        </div>
+      </div>
+      <div className={style.videoWrapper}>
+        <video src={homeVideo} autoPlay muted loop className={style.bgVideo} />
+      </div>
+    </div>
+  );
+};
 
 export default Detail;
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -5,7 +5,6 @@ const {
 } = require("../handlers/charHandler");
 const { createUser, loginUser } = require("../handlers/userHandler");
 const { postFav, deleteFav } = require("../controllers/handleFavorites");
-const { Log } = require("../controllers/login");
 
 const router = require("express").Router();
 
@@ -14,7 +13,6 @@ router.get("/character/:id", findCharByID); // Funciona ✅
 router.post("/character/addcustom", addCustomChar); // Funciona ✅
 router.post("/register", createUser); // Funciona ✅
 router.post("/loginNew", loginUser); // Funciona ✅
-router.get("/login", Log); 
 router.post("/fav", postFav);
 router.delete("/fav/:id", deleteFav);
 
