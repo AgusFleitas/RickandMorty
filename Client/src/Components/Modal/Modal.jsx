d4dcbe3e052133019c3202f5825a9369240288b3
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-01-03 19:04:07 +0100

    Modal LogOut listo

--- Client/src/Components/Modal/Modal.jsx
+++ Client/src/Components/Modal/Modal.jsx
@@ -1,17 +1,29 @@
+import "boxicons";
+
 import style from "./Modal.module.css";
 
-const Modal = ({ title, message, actionName, actionFunc, cancelFunc}) => {
+const Modal = ({ title, message, actionName, actionFunc, cancelFunc }) => {
   return (
     <div className={style.overlay}>
       <div className={style.modalwrapper}>
         <h3>{title}</h3>
+        <box-icon
+          type='solid'
+          name='error'
+          size='md'
+          color='rgb(217, 187, 100)'
+        ></box-icon>
         <div className={style.message}>
           <p>{message}</p>
         </div>
-          <div className={style.buttons}>
-            <button onClick={actionFunc}>{actionName}</button>
-            <button onClick={cancelFunc}>Cancel</button>
-          </div>
+        <div className={style.buttons}>
+          <button className={style.actionbutton} onClick={actionFunc}>
+            {actionName}
+          </button>
+          <button className={style.cancelbutton} onClick={cancelFunc}>
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   );
